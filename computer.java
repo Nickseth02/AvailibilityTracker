5fd99e3976884cf1d0fd0632ae6c0819865650a2
Nishchal Jain <nickseth02@gmail.com>
2026-04-10 14:58:28 +0530

    Delete computer.java

--- computer.java
+++
@@ -1,8 +0,0 @@
-mport java.util.Scanner;
-
-public class AvailabilityTrackerLogic {
-    public static void main(String[] args) {
-        // Hello message for the logic demo
-        System.out.println("Hello! This is the core logic for the Availability Tracker.");
-	}
-}
