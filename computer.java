629a8459e4ab1fe731aababffdb60e26ebfdd960
Shravani Pande <shravanipande26@gmail.com>
2026-04-10 13:26:29 +0530

    created a trial file computer

---
+++ computer.java
@@ -0,0 +1,8 @@
+mport java.util.Scanner;
+
+public class AvailabilityTrackerLogic {
+    public static void main(String[] args) {
+        // Hello message for the logic demo
+        System.out.println("Hello! This is the core logic for the Availability Tracker.");
+	}
+}
